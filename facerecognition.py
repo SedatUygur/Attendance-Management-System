2724e42d420bc8c3b89bf8a74f36afa0ea4b00a4
Sedat Uygur <sedat.uygur@outlook.com>
2024-09-07 18:13:02 +0300

    compare faces to check if both ones are identical

--- facerecognition.py
+++ facerecognition.py
@@ -7,6 +7,9 @@ face_location = face_recognition.face_locations(image_rgb)[0] # finds face locat
 copy_image_rgb = image_rgb.copy()
 #cv2.rectangle(image, start_point, end_point, color, thickness) 
 cv2.rectangle(copy_image_rgb, (face_location[3], face_location[0]),(face_location[1], face_location[2]), (255,225,25), 2) # draws rectangle
+copy_face_encoding = face_recognition.face_encodings(copy_image_rgb)[0]
+face_encoding = face_recognition.face_encodings(image_rgb)[0]
+print(face_recognition.compare_faces([copy_face_encoding],face_encoding))
 cv2.imshow('copy', copy_image_rgb)
 cv2.imshow('elon',image_rgb)
 cv2.waitKey(0)
