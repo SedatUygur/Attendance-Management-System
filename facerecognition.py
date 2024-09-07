2bc5ca6b3c114c8f0a28f2c232546eea79ef08d9
Sedat Uygur <sedat.uygur@outlook.com>
2024-09-07 17:50:03 +0300

    find the face location & draw bounding box

--- facerecognition.py
+++ facerecognition.py
@@ -2,7 +2,11 @@ import cv2
 import numpy as np
 import face_recognition
 image_bgr = face_recognition.load_image_file('elon_musk.jpg') # loads image in the form of BGR
-image_rgb = cv2.cvtColor(image_bgr,cv2.COLOR_BGR2RGB) # convert image into RGB
-cv2.imshow('bgr', image_bgr) # show image in the form of BGR
-cv2.imshow('rgb', image_rgb) # show image in the form of RGB
+image_rgb = cv2.cvtColor(image_bgr,cv2.COLOR_BGR2RGB) # converts image into RGB
+face_location = face_recognition.face_locations(image_rgb)[0] # finds face location
+copy_image_rgb = image_rgb.copy()
+#cv2.rectangle(image, start_point, end_point, color, thickness) 
+cv2.rectangle(copy_image_rgb, (face_location[3], face_location[0]),(face_location[1], face_location[2]), (255,225,25), 2) # draws rectangle
+cv2.imshow('copy', copy_image_rgb)
+cv2.imshow('elon',image_rgb)
 cv2.waitKey(0)
